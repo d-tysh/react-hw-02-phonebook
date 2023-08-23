b26567ae8df85517d8b75b2c14397fd9ca5d2b0e
d-tysh <39271488+d-tysh@users.noreply.github.com>
2023-08-23 19:51:33 +0300

    commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { combineReducers, configureStore, createStore } from "@reduxjs/toolkit";
+import { combineReducers, createStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filterReducer } from "./filterSlice";
 import storage from 'redux-persist/lib/storage';
