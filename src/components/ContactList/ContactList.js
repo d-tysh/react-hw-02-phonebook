9d8f02f7a583a42192a3ca9df9ac8a62e73b8e4f
d-tysh <39271488+d-tysh@users.noreply.github.com>
2023-08-28 15:09:46 +0300

    commit

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,18 +1,14 @@
 import { useDispatch, useSelector } from "react-redux";
 import { deleteContact } from "redux/operations";
-import { selectContacts, selectFilter } from "redux/selectors";
+import { selectVisibleContacts } from "redux/selectors";
 
 export const ContactList = () => {
-    const contacts = useSelector(selectContacts);
-    const filter = useSelector(selectFilter);
     const dispatch = useDispatch();
+    const visibleContacts = useSelector(selectVisibleContacts);
 
     const deleteContactById = (contactId) => {
         dispatch(deleteContact(contactId));
     }
-
-    const normalizeFilter = filter.toLowerCase();
-    const visibleContacts = contacts.filter(contact => contact.name.toLowerCase().includes(normalizeFilter));
 
     return (
         <ul style={{'listStyle': 'none'}}>
